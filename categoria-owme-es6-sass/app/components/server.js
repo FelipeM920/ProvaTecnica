96a3ea222bd4500ea043ed6f042d28ad2b830922
Felipe Moraes <felipemoraes920@gmail.com>
2020-01-12 01:18:55 -0300

    products aligns

--- categoria-owme-es6-sass/app/components/server.js
+++ categoria-owme-es6-sass/app/components/server.js
@@ -1,14 +1,12 @@
 class Server {
 
-   async loadProducts() {
-       try {
-      const resp = await fetch('https://raw.githubusercontent.com/ArezzoCo/ecommerce-prova-tecnica/master/front-end/categoria-owme-es6-sass/mock-products.json')
-            // .then(response => response.json())
-            // .then(data => console.log(JSON.stringify(data)))
+    async loadProducts() {
+        try {
+            const resp = await fetch('https://raw.githubusercontent.com/ArezzoCo/ecommerce-prova-tecnica/master/front-end/categoria-owme-es6-sass/mock-products.json')
             return resp.json();
-       } catch(err) {
-           console.log(err)
-       }
+        } catch (err) {
+            console.log(err)
+        }
 
     }
 }
--- categoria-owme-es6-sass/app/main.js
+++ categoria-owme-es6-sass/app/main.js
@@ -3,4 +3,26 @@ import { Server } from '../app/components/server.js';
 const server = new Server();
 
 server.loadProducts()
-.then(data => console.log(data.products))
+    .then(data => mapToHtml(data.products))
+
+function mapToHtml(products) {
+    console.log(products[0])
+    let productsDiv = document.getElementById('products');
+    let template = '<ul class="container-products">';
+    products.forEach(product => {
+        template +=
+            `<li>
+                <picture>
+                    <img srcset="${product.image}">
+                </picture>
+                <p>
+                    ${product.name}
+                </p>
+                <p>
+                    ${product.price}
+                </p>
+            </li>`
+    });
+    template += '</ul>';
+    productsDiv.insertAdjacentHTML('beforeend', template);
+}
